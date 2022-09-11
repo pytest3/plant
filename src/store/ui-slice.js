8dbf4044c1c13b93c1a6a31769225ebd05225776
pytest <pytest514@gmail.com>
2022-09-11 21:47:06 +0800

    Added UI slice

--- src/store/ui-slice.js
+++ src/store/ui-slice.js
@@ -1,22 +1,41 @@
 import { createSlice } from "@reduxjs/toolkit";
+
 const initialState = {
   status: "",
   title: "",
   message: "",
 };
 
-const uiSlice = createSlice({
+export const uiSlice = createSlice({
   name: "ui",
   initialState,
   reducers: {
     sendNotification: (state, action) => {
-      if (action.payload.type === "sending") {
+      if (action.payload === "sending") {
+        console.log("sending");
         return {
           status: "sending",
           title: "Sending!",
           message: "Sending plants data..",
         };
+      } else if (action.payload === "success") {
+        console.log("success");
+        return {
+          status: "success",
+          title: "Success!",
+          message: "Sent plants data!",
+        };
+      } else if (action.payload === "error") {
+        console.log("error");
+        return {
+          status: "error",
+          title: "Error!",
+          message: "Failed to send plants data",
+        };
       }
     },
   },
 });
+
+export default uiSlice.reducer;
+export const { sendNotification } = uiSlice.actions;
