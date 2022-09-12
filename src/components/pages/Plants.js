24bd549c92a50d34968f207a0a4d2a835d8b09f1
pytest <pytest514@gmail.com>
2022-09-12 21:09:00 +0800

    Remove old code

--- src/components/pages/Plants.js
+++ src/components/pages/Plants.js
@@ -1,49 +1,17 @@
-import React, { useEffect } from "react";
-import { useDispatch } from "react-redux";
-import useHttp from "../../hooks/use-http";
-import { getAllPlants, refreshPlantData } from "../../lib/api";
-import { changeLastWateredToToday } from "../../store/plants-slice";
+import React from "react";
 import PlantList from "../plants/PlantList";
 import LoadingSpinner from "../ui/LoadingSpinner";
 
-let initialRun = true;
-
 const Plants = (props) => {
-  const { data: allPlantsData, sendRequest, status } = useHttp();
-  const dispatch = useDispatch();
+  // if (status === "pending") {
+  //   return (
+  //     <div className="centered">
+  //       <LoadingSpinner />;
+  //     </div>
+  //   );
+  // }
 
-  // Re-calculate days since last watered when app first runs
-  useEffect(() => {
-    if (initialRun) {
-      console.log("refreshing data..");
-      sendRequest(refreshPlantData);
-      initialRun = false;
-    }
-  }, [sendRequest]);
-
-  useEffect(() => {
-    sendRequest(getAllPlants);
-  }, [sendRequest]);
-
-  const updatedToTodayHandler = () => {
-    // sendRequest(getAllPlants);
-    return;
-  };
-
-  if (status === "pending") {
-    return (
-      <div className="centered">
-        <LoadingSpinner />;
-      </div>
-    );
-  }
-
-  return (
-    <PlantList
-      allPlantsData={allPlantsData}
-      onUpdatedToToday={updatedToTodayHandler}
-    />
-  );
+  return <PlantList />;
 };
 
 export default Plants;
