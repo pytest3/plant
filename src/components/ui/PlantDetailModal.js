d5c70d7e8d9f45dc6fcb8d20f7a5fa3fc8e495f0
pytest <pytest514@gmail.com>
2022-09-18 23:17:58 +0800

    Initial commit

---
+++ src/components/ui/PlantDetailModal.js
@@ -0,0 +1,58 @@
+import { Fragment, useState } from "react";
+import ReactDOM from "react-dom";
+import Modal from "./Modal";
+import Button from "./Button";
+import { useDispatch, useSelector } from "react-redux";
+import { toggleModal } from "../../store/ui-slice";
+import EditPlantForm from "../plants/EditPlantForm";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrashCan } from "@fortawesome/free-regular-svg-icons";
+import classes from "./PlantDetailModal.module.css";
+import { deletePlant } from "../../store/plants-slice";
+import DeleteConfirmationForm from "../plants/DeleteConfirmationForm";
+
+const PlantDetailModal = () => {
+  const [askToDelete, setAskToDelete] = useState(false);
+  const [deleteConfirmed, setDeleteConfirmed] = useState(false);
+
+  const dispatch = useDispatch();
+  // const deleteConfirmation = useSelector(
+  //   (state) => state.plants.deleteConfirmation
+  // );
+  const plantId = useSelector((state) => state.plants.currentPlantId);
+  const plantData = useSelector((state) => state.plants.allPlants).find(
+    (plant) => plant.id === plantId
+  );
+  const deletePlantHandler = () => {
+    setAskToDelete(true);
+  };
+
+  const deleteIcon = (
+    <FontAwesomeIcon
+      icon={faTrashCan}
+      className={classes.trash}
+      onClick={deletePlantHandler}
+    />
+  );
+
+  let plantDetailModalContent = (
+    <Modal header={plantData.name} plantInfo={plantData} icon={deleteIcon}>
+      <EditPlantForm name={plantData.name} />
+    </Modal>
+  );
+
+  if (askToDelete) {
+    plantDetailModalContent = (
+      <Modal header="Delete plant">
+        <DeleteConfirmationForm name={plantData.name} id={plantId} />
+      </Modal>
+    );
+  }
+
+  return ReactDOM.createPortal(
+    plantDetailModalContent,
+    document.getElementById("overlay-root")
+  );
+};
+
+export default PlantDetailModal;
