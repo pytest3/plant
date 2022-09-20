7373ab568fd11c571b1fea9c646ab58552ac7fcd
pytest <pytest514@gmail.com>
2022-09-21 00:24:28 +0800

    Add photos reducer slice

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import plantsReducer from "./plants-slice";
 import uiReducer from "./ui-slice";
+import photosReducer from "./photos-slice";
 
 const store = configureStore({
   reducer: {
     plants: plantsReducer,
     ui: uiReducer,
+    photos: photosReducer,
   },
 });
 
