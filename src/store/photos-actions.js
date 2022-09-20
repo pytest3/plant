5129bb0ab10caae4819c65ae574a3b4c25c8b457
pytest <pytest514@gmail.com>
2022-09-21 00:23:28 +0800

    Initial commit

---
+++ src/store/photos-actions.js
@@ -0,0 +1,26 @@
+import { ref, listAll, getDownloadURL, getMetadata } from "firebase/storage";
+import { useDispatch } from "react-redux";
+import { storage } from "../firebase";
+import { refreshPhotos } from "./photos-slice";
+
+export const downloadPhotoInfo = () => {
+  return async () => {
+    const dispatch = useDispatch();
+    const imageListRef = ref(storage, "images/");
+    // listAll returns a refrence object
+    // {items: [array of photos' reference], prefixes: [array of photos' prefix]}
+    const { items: photoItems } = await listAll(imageListRef);
+    const photoUrls = await Promise.all(
+      photoItems.map(async (item) => await getDownloadURL(item))
+    );
+    const photoIds = await Promise.all(
+      photoItems.map((item) =>
+        getMetadata(item).then((metaData) => metaData.name)
+      )
+    );
+    const photoList = photoUrls.map((url, index) => {
+      return { id: photoIds[index], url };
+    });
+    dispatch(refreshPhotos(photoList));
+  };
+};
