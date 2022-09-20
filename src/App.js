28af59237059e9cda270d0c7afb095fa93d73ee4
pytest <pytest514@gmail.com>
2022-09-21 00:23:17 +0800

    Shift firebase login to firebase-actions

--- src/App.js
+++ src/App.js
@@ -8,10 +8,8 @@ import { useSelector, useDispatch } from "react-redux";
 import { useEffect } from "react";
 import { getPlantData, sendPlantData } from "./store/plants-actions";
 import LoadingSpinner from "./components/ui/LoadingSpinner";
-import { storage } from "./firebase";
-import { ref, listAll, getDownloadURL, getMetadata } from "firebase/storage";
-import { useState } from "react";
 import Photos from "./components/pages/Photos";
+import { downloadPhotoInfo } from "./store/photos-actions";
 
 let initialRun = true;
 
@@ -19,34 +17,15 @@ function App() {
   const plants = useSelector((state) => state.plants.allPlants);
   const dispatch = useDispatch();
   const notification = useSelector((state) => state.ui);
-  const [imageList, setImageList] = useState([]);
-  const imageListRef = ref(storage, "images/");
+  const allPhotos = useSelector((state) => state.photos.allPhotos);
 
   useEffect(() => {
     dispatch(getPlantData());
-    console.log("here");
   }, [dispatch]);
 
   useEffect(() => {
-    // listAll returns a refrence object
-    // {items: [array of photos' reference], prefixes: [array of photos' prefix]}
-    const getPhotoData = async () => {
-      const { items: photoItems } = await listAll(imageListRef);
-      const photoUrls = await Promise.all(
-        photoItems.map(async (item) => await getDownloadURL(item))
-      );
-      const photoIds = await Promise.all(
-        photoItems.map((item) =>
-          getMetadata(item).then((metaData) => metaData.name)
-        )
-      );
-      const photoList = photoUrls.map((url, index) => {
-        return { id: photoIds[index], url };
-      });
-      setImageList(photoList);
-    };
-    getPhotoData();
-  }, [imageListRef]);
+    downloadPhotoInfo();
+  }, [dispatch]);
 
   useEffect(() => {
     if (initialRun) {
@@ -70,7 +49,7 @@ function App() {
       <Routes>
         <Route path="*" element={<Navigate to="/plants" replace />}></Route>;
         <Route path="/plants" element={<Plants />}></Route>;
-        <Route path="/photos" element={<Photos images={imageList} />}></Route>;
+        <Route path="/photos" element={<Photos images={allPhotos} />}></Route>;
         <Route path="/plants/:plantId" element={<PlantDetail />}></Route>
         <Route path="/new-plant" element={<NewPlantForm />}></Route>
       </Routes>
